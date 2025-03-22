cf40fc98ae2a7f6887ab1820113da84c71c42ef5
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-22 19:05:19 +0300

    add some test

--- src/test/java/site/easy/to/build/crm/CrmApplicationTests.java
+++ src/test/java/site/easy/to/build/crm/CrmApplicationTests.java
@@ -1,13 +1,33 @@
 package site.easy.to.build.crm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import site.easy.to.build.crm.DTO.BudgetDTO;
+import site.easy.to.build.crm.service.budget.BudgetService;
+
+import java.util.List;
 
 @SpringBootTest
 class CrmApplicationTests {
 
+	@Autowired
+	BudgetService budgetService;
+
 	@Test
 	void contextLoads() {
+
+		List<BudgetDTO> budgetDTOS =  budgetService.getBudgetsAfterExpense(43);
+		for (int i = 0; i < budgetDTOS.size(); i++) {
+			System.out.println(budgetDTOS.get(i).getBudgetId());
+			System.out.println(budgetDTOS.get(i).getTitle());
+			System.out.println(budgetDTOS.get(i).getInitialAmount());
+			System.out.println(budgetDTOS.get(i).getCurrentAmount());
+			System.out.println(budgetDTOS.get(i).getStartDate());
+			System.out.println(budgetDTOS.get(i).getEndDate());
+			System.out.println(budgetDTOS.get(i).getCustomerId());
+
+		}
 	}
 
 }
