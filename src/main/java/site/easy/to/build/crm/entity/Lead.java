86877e79400c79c5a7dcea72a565b0e6654a476d
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-25 17:11:02 +0300

    Add cascade option to ManyToOne relationship for Expense in Lead and Ticket entities

--- src/main/java/site/easy/to/build/crm/entity/Lead.java
+++ src/main/java/site/easy/to/build/crm/entity/Lead.java
@@ -61,7 +61,7 @@ public class Lead {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "expense_id")
     private Expense expense;
 
